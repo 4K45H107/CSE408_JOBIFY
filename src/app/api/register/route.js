852dd4aefea7d5554c7058ce899eb0216cb1aea7
Rayan <rayan.islam.2586@gmail.com>
2024-01-27 03:00:38 +0600

    added get method for register route

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -5,16 +5,31 @@ import { connetToDb } from "../../../../lib/utils"
 
 export const POST= async (request) => {
     try {
+        console.log("inside register route");
         connetToDb();
         const data={
-            username:"test",
-            name:"test",
-            email:"test@gmai.com",
+            username:"test3",
+            name:"test3",
+            email:"test3@gmail.com",
             password:"1",
         }
         const user = await User.create(data);
 
-        return NextResponse.json({user},{status:200});
+        return NextResponse.json(user,{status:200});
+    } catch (error) {
+        console.log(error);
+        return NextResponse.error(error,{status:500});   
+    }
+
+}
+
+export const GET= async (request) => {
+    try {
+        connetToDb();
+        
+        const users = await User.find({});
+
+        return NextResponse.json(users,{status:200});
     } catch (error) {
         console.log(error);
         return NextResponse.error(error,{status:500});   
