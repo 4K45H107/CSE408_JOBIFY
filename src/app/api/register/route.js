c2f470ffa6943998902504578e63b59e1e3e1c43
rayan <rayan.islam.2586@gmail.com>
2024-01-27 23:07:24 +0600

    merge conflict

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,39 +1,18 @@
-import { User } from "../../../../lib/models"
 import { NextResponse } from "next/server";
-import { connetToDb } from "../../../../lib/utils"
+ import { User } from "../../../../lib/models";
+import { connetToDb } from "../../../../lib/utils";
 
 
 export const POST= async (request) => {
     try {
-        
+        const data = await request.json();
+        console.log(data);
         console.log("inside register route");
         connetToDb();
-        const data={
-            username:"tested",
-            email:"tested@gmail.com",
-            password:"1",
-            fullname:"test4",
-            profile:{
-                location:"dhaka",
-                skills:["java","python","react"],
-                education: "bachelor",
-            },
-            contact:{
-                phone:"01835286271",
-            },
-            privacySettings:{
-                notification: true,
-                publicProfile: true, 
-            },
-            job_preferences:{
-                locations: "dhaka",
-                salary_range:"20k-100k",
-                job_type:["developer"],
-            }
-        }
+        
         const user = await User.create(data);
-
         return NextResponse.json(user,{status:200});
+        //return NextResponse.json(user,{status:200});
     } catch (error) {
         console.log(error);
         return NextResponse.error(error,{status:500});   
@@ -45,9 +24,9 @@ export const GET= async (request) => {
     try {
         connetToDb();
         
-        const users = await User.find({});
+        // const users = await User.find({});
 
-        return NextResponse.json(users,{status:200});
+        // return NextResponse.json(users,{status:200});
     } catch (error) {
         console.log(error);
         return NextResponse.error(error,{status:500});   
