7c7ea6d823526c066455309ed074403c3f344d29
4K45H107 <fahad11.ahmed11@gmail.com>
2024-01-29 06:20:44 +0600

    front-end explore and login2

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,4 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios"
+import { useRouter } from "next/navigation";
+
 const LoginPage = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      username,
+      password,
+    };
+
+    try {
+      const res = await axios.post("/api/login/user", user);
+      const data = res.data;
+      console.log(data);
+      router.push("/employee/explore")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex h-full w-full items-center justify-center">
       <form className="flex flex-col border px-6 py-10">
@@ -7,14 +36,22 @@ const LoginPage = () => {
           className="border rounded py-3 px-2 mb-3"
           type="text"
           placeholder="User Name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         ></input>
         <lebel>Enter your Password:</lebel>
         <input
           className="border rounded py-2 px-2 mb-3"
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <button className="bg-gray-700 rounded text-white px-4 py-1 active:bg-slate-600 w-32 mx-auto">
+        <button
+          type="submit"
+          onClick={handleLogin}
+          className="bg-gray-700 rounded text-white px-4 py-1 active:bg-slate-600 w-32 mx-auto"
+        >
           Login
         </button>
       </form>
