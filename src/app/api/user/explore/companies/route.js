7a51b2d996f0ae6b06d52616b1f82a079a2af079
unknown <1905116@ugrad.cse.buet.ac.bd>
2024-01-29 14:11:51 +0600

    searching companoies by user done

---
+++ src/app/api/user/explore/companies/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { Companies } from "../../../../../../lib/models";
+import { connetToDb } from "../../../../../../lib/utils";
+
+export const GET = async (request) =>{
+    try{
+        connetToDb();
+        // const data = await request.json();
+        const name = "Google";
+        console.log(name);
+
+        const company = await Companies.findOne({name: name});
+        console.log(company.name);
+        if(!company){
+            console.log("no company");
+            return NextResponse.json({message: "no such company"}, {status: 401});
+        }
+        else{
+            return NextResponse.json(company, {status: 200});
+        }
+    }catch(error){
+        console.log("Error:", error);
+        return NextResponse.error(error, {status: 500});
+
+    }
+}
