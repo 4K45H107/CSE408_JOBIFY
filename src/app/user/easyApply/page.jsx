6c883e99adf8bedecca600648562ff6c974f9486
4K45H107 <fahad11.ahmed11@gmail.com>
2024-03-04 02:32:55 +0600

    mcq + apply show control

--- src/app/user/easyApply/page.jsx
+++ src/app/user/easyApply/page.jsx
@@ -13,12 +13,31 @@ const easyApply = () => {
   const [pdf, setPdf] = useState("");
   const { role, userId } = useContext(AuthContext);
 
+  const [type, setType] = useState();
+
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const jobId = searchParams.get("jobId");
 
   console.log(jobId);
+
+  const handleupload = async () => {
+    const data = {
+      user_id: userId,
+      job_id: jobId
+    };
+
+    try {
+      const res = await axios.patch(
+        `/api/user/profile/picture?userId=${userId}`,
+        data
+      );
+    } catch (error) {
+      console.log(error);
+    } 
+  };
+
 
   const { data: profile, isLoading } = useSWR(
     `/api/user/profile?userId=${userId}`,
@@ -81,37 +100,50 @@ const easyApply = () => {
   };
 
   if (!isLoading) {
-    return (
-      <div className="flex justify-center items-start h-screen">
-        <div className=" h-full w-4/5 p-6 border-gray-300 ml-12 mt-12">
-          <div className="flex flex-col">
-            <div className="flex gap-x-4">
-              <div className="flex justify-center my-4 w-40 bg-gray-800 rounded">
-                <button
-                  type="submit"
-                  onClick={() => setUploadShowCv(true)}
-                  className="w-40 text-white px-4 py-3 active:bg-slate-600 mx-auto"
-                >
-                  Upload
-                </button>
+    if (!type) {
+      return (
+        <div className="flex justify-center items-start h-screen">
+          <div className=" h-full w-4/5 p-6 border-gray-300 ml-12 mt-12">
+            <div className="flex flex-col">
+              <div className="flex gap-x-4">
+                <div className="flex justify-center my-4 w-40 bg-gray-800 rounded">
+                  <button
+                    type="submit"
+                    onClick={() => setUploadShowCv(true)}
+                    className="w-40 text-white px-4 py-3 active:bg-slate-600 mx-auto"
+                  >
+                    Upload
+                  </button>
+                </div>
+                <div className="flex justify-center my-4 w-40 bg-gray-800 rounded">
+                  <button
+                    type="submit"
+                    onClick={handleNext}
+                    className="w-40 text-white px-4 py-3 active:bg-slate-600 mx-auto"
+                  >
+                    Next
+                  </button>
+                </div>
               </div>
-              <div className="flex justify-center my-4 w-40 bg-gray-800 rounded">
-                <button
-                  type="submit"
-                  onClick={handleNext}
-                  className="w-40 text-white px-4 py-3 active:bg-slate-600 mx-auto"
-                >
-                  Next
-                </button>
-              </div>
-            </div>
-            {uploadShowCv && <UploadFile imageUrl={pdf} setImageURL={setPdf} />}
+              {uploadShowCv && (
+                <UploadFile imageUrl={pdf} setImageURL={setPdf} />
+              )}
 
-            {pdf && <iframe src={pdf} className="h-screen w-100"></iframe>}
+              {pdf && <iframe src={pdf} className="h-screen w-100"></iframe>}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="">
+          <p className="text-4xl bold">
+            {" "}
+            You have already Applied to this Job!!!
+          </p>
+        </div>
+      );
+    }
   }
 };
 
