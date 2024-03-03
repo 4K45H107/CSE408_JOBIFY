572077846cb4d27a1e090da42f2bcecd406641eb
Rayan <rayan.islam.2586@gmail.com>
2024-03-03 22:24:02 +0600

    easy apply done

--- src/app/api/easyApply/route.js
+++ src/app/api/easyApply/route.js
@@ -1,21 +1,39 @@
 import { NextResponse } from "next/server";
+import { Applications, Jobs, Notification, Tests, User } from "../../../../lib/models";
 import { connetToDb } from "../../../../lib/utils";
-import { Applications , Jobs , Notification} from "../../../../lib/models";
 
 export const POST = async (request) => {
     try{
         connetToDb();
         
-        if(id == null){
-            return NextResponse.json({message: "User not found"}, {status: 404});
-        }
+        
         const data = await request.json();
         const status =  "ongoing";
         try {
+            const user= await User.findById(data.user_id);
+            if(user.cv === "N/A"){
+                return NextResponse.json({message: "Please upload your CV before applying for a job"}, {status: 400});
+            }
+            const findApplication = await Applications.find({'user_id': data.user_id, 'job_id': data.id});
+            if(findApplication.length > 0){
+                return NextResponse.json({message: "Application already exists"}, {status: 400});
+            }
             const application = await Applications.create({'user_id': data.user_id, 'job_id': data.id, 'status': status});
             const job = await Jobs.findById(data.job_id);
             if(job.skill_test){
-                const notification = await Notification.create({'user_id': data.user_id, 'message': `You have a new skill test to take for the job ${job.title}` , 'job_id': data.job_id, 'status': "unread"});
+                const message = {
+                    "name":job.company,
+                    "description": `You have a new skill test to take for the job titled \"${job.title}\" \n
+                    for the company `,
+                }
+                const messageEmployer = {
+                    "name":user.fullname,
+                    "description": `This candidate has applied for the job titled \"${job.title}\" `,
+                }
+
+                const tests = await Tests.create({'user_id': data.user_id, 'job_id': data.job_id, 'given': false});
+                const notification = await Notification.create({'user_id': data.user_id, 'message': message , 'job_id': data.job_id, 'type': "regular","read": false});
+                const notEmployer = await Notification.create({'employer_id': job.provider, 'message': messageEmployer , 'job_id': data.job_id, 'type': "applied","read": false});
             }
             return NextResponse.json(application, {status: 200});
             
---
+++ src/app/api/interview/user/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { connetToDb } from "../../../../../lib/utils";
+
+export const POST = async (request) => {
+  try {
+    connetToDb();
+
+    // Extract the ID from the request parameters
+
+    const data = await request.json();
+    // Find the user by ID
+
+    const id = data.companyId;
+
+    const companies = await Companies.findByIdAndUpdate(id, data, {"admin":data.admin}, { new: true });
+    
+    return NextResponse.json(companies, { status: 200 });
+
+    // console.log(job_preferences);
+    // Return a response if needed
+    // return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.log("Error:", error);
+    return NextResponse.error(error, { status: 500 });
+  }
+};
