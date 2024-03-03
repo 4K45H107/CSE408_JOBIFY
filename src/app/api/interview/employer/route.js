5984ee1d1be10606fcb4b6f3ff76543b5542d022
Rayan <rayan.islam.2586@gmail.com>
2024-03-04 05:31:46 +0600

    interview scheduling for employer

--- src/app/api/interview/employer/route.js
+++ src/app/api/interview/employer/route.js
@@ -1,5 +1,13 @@
+// {
+//   job_id: "",
+//   user_id: "",
+//   employer_id: "",
+// }
+
+
 import { NextResponse } from "next/server";
 import { connetToDb } from "../../../../../lib/utils";
+import { Interviews, Notification } from "../../../../../lib/models";
 
 export const POST = async (request) => {
   try {
@@ -9,11 +17,15 @@ export const POST = async (request) => {
 
     const data = await request.json();
     // Find the user by ID
-
-    const id = data.companyId;
-
-    const companies = await Companies.findByIdAndUpdate(id, data, {"admin":data.admin}, { new: true });
-    
+    const interview=await Interviews.create(data);
+    const job= await Job.findById(data.job_id);
+    const message={
+      name: "New Interview",
+      designation: "Interview",
+      description: `New Interview has been scheduled for the job title ${job.title} at ${job.company}`
+    }
+    await Notification.create({user_id: data.user_id, message: message, type: "interview",read:false});
+    await Notification.create({employer_id: data.employer_id, message: message, type: "interview",read:false});
     return NextResponse.json(companies, { status: 200 });
 
     // console.log(job_preferences);
@@ -24,3 +36,20 @@ export const POST = async (request) => {
     return NextResponse.error(error, { status: 500 });
   }
 };
+
+
+export const GET = async (request) => {
+  try {
+    connetToDb();
+
+    // Extract the ID from the request parameters
+    const url= new URL(request.url);
+    const id = url.searchParams.get("employerId");
+    // Find the user by ID
+    let interviews=[];
+    
+  }catch{
+
+  } 
+}
+
--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { connetToDb } from "../../../../lib/utils";
 import { Tests } from "../../../../lib/models";
+import { connetToDb } from "../../../../lib/utils";
 
 export const POST = async (request) => {
   try {
